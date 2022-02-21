10b8baec1e220a5021811d9f15ff7fc332a80159
Erick Rodriguez <erickrod54@gmail.com>
2022-02-21 05:14:02 +0000

    Nuggets react version 2 for map method - Features: Building states to toggle people, and ages, pending show ages, and ages by twice

--- nuggets-react-app/src/App.js
+++ nuggets-react-app/src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import MapMethod from '../src/components/map-method-component'
 import './App.css';
 
+/**Nuggets react version 2 - map method - Features: 
+ * 
+ *     ----> Building states to toggle 'people', and 
+ *           ages    
+ *   
+ *   Note: Pending features show ages, and ages by 
+ *   twice
+ * 
+ */
+
 function App() {
+  /**i build this state for toggle and show the people */
+  const [show, setShow] = useState(false)
+  /**i build this state to toogle the ages options */
+  const [ ages, setAges ] = useState(false)
+  /**i build this state for oldAges */
+  const [  oldAges, setOldAges ] = useState(false)
+
+
   return (
     <div className="App">
-      <MapMethod />
+      <>
+      <button onClick={()=> setShow(!show)}>Show People</button>
+      { show && <MapMethod />}
+      <button onClick={() => setAges(!ages)}>Show Ages </button>
+      { ages && 
+      <div>
+        <button onClick={() => setOldAges(!oldAges)}>Show Old Ages</button>
+        <button>Show Ages by twice</button>
+      </div>}
+      </>
+      
     </div>
   );
 }
