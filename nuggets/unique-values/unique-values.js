8e11cb28c9f2f25d8f67d0539244258836d18cab
Erick Rodriguez <erickrod54@gmail.com>
2022-02-22 05:07:43 +0000

    Unique values app version 1 - vanilla JavaScript - unique values app - Features: setting the data for the menu, maping the menu data, and creating a set of unique values using new set functionality

--- nuggets/unique-values/unique-values.js
+++ nuggets/unique-values/unique-values.js
@@ -1,1 +1,39 @@
-/**Here is where the unique values practice is going to be make */
+//Unique Values data
+const menu = [
+    {
+     name: 'pancakes',
+     category: 'breakfast'
+    },
+    {
+        name: 'burger',
+        category: 'lunch'
+    },
+    {
+        name: 'steak',
+        category: 'dinner'
+    },
+    {
+        name: 'bacon',
+        category: 'breakfast'
+    },
+    {
+        name: 'eggs',
+        category: 'breakfast'
+    },
+    {
+        name: 'pasta',
+        category: 'dinner'
+    }
+];
+/**i spread it, and send the new set to create an array
+ * -otherwise if i do it like: 
+ * 
+ * 'new set(menu.map((item) => item.category))'
+ * 
+ * will be resulting an object
+ */
+const categories = [ all, ...new set(menu.map((item) => item.category))]
+/**i spread all as a new category and then i map on menu to send the 
+ * category
+ */
+console.log(categories)
