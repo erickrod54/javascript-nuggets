96b4a82524307ba3826ecbfdd8a788df2a8e8b4a
Erick Rodriguez <erickrod54@gmail.com>
2022-02-24 11:28:21 +0000

    Nuggets react version 4 - map method - Features: Building getAgesByTwo

--- nuggets-react-app/src/App.js
+++ nuggets-react-app/src/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import MapMethod from '../src/components/map-method-component';
-import data from './data';
+import { data } from './data';
 import './App.css';
 
-/**Nuggets react version 3 - map method - Features: 
+/**Nuggets react version 4 - map method - Features: 
  * 
- *     ----> Building getAges to get the ages from 
- *          the data    
- *   
- *   Note: Pending features ages by 
- *   twice
+ *     ----> Building getAgesByTwo()
  * 
  */
 
@@ -17,10 +13,10 @@ function App() {
   
   /**i build this state for toggle and show the people */
   const [show, setShow] = useState(false)
-  /**i build this state to toogle the ages options */
-  const [ ages, setAges ] = useState(false)
   /**i build this state for oldAges */
   const [  oldAges, setOldAges ] = useState(false)
+  /**this is the state for the current age */
+  const [  age, setAge ] = useState(false)
 
  /**here i get the current age */
   const getAges = () => {
@@ -34,21 +30,29 @@ function App() {
 
   }
   
+  /**here i get the ages by 2 */
+  const getAgesByTwo = () => {
+    const ByTwo = data.map((person) => {
+     return(
+       <h1>{person.age * 2}</h1>
+      )
+    })
+
+    return ByTwo
+
+ }
 
   return (
     <div className="App">
       <>
       <button onClick={()=> setShow(!show)}>Show People</button>
       { show && <MapMethod />}
-      <button onClick={() => setAges(!ages)}>Show Ages </button>
-      { ages && 
-      <div>
         {/**here i trigger to getAges by onClick */}
         <button onClick={() => setOldAges(!oldAges)}>Show Old Ages</button>
         { oldAges && getAges()}
        {/**pending building by twice */}
-        <button>Ages By Twice</button>
-      </div>}
+        <button onClick={() => setAge(!age)}>Ages By Twice</button>
+        { age && getAgesByTwo() }
       </>
       
     </div>
