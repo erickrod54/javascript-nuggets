094385586d95c422210acc031383f8e744265e65
Erick Rodriguez <erickrod54@gmail.com>
2022-02-27 05:29:04 +0000

    Object Keys - vanilla JavaScript version 2 - Features: set a key object with square noation - checkout appState, keyName, and app-

--- nuggets/object-keys/object-keys.js
+++ nuggets/object-keys/object-keys.js
@@ -24,3 +24,28 @@ console.log(person['name']) // 'john'
 
 //now to set a new key for square barcket 
 
+let appState = 'loading';
+
+//-i can test to set appState value *uncomment to test*-
+//appState = 'error'
+
+/**here i set the key
+ * -'[appState]' with the square notation-
+ */
+
+//i can set a new key with value and add it to
+//the app 'object'
+
+//here i set the key name for the second
+//item for 'app' object
+const keyName = 'computer';
+
+const app = {
+    [appState]: false
+}
+
+//here i set the value for the keyName
+app[keyName] = 'apple'
+
+console.log(app) //prompt me: { loading : true, computer : apple}
+
