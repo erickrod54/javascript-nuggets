e04b46ca4cb1bb6fb554fd1b64d6f539e123a513
Erick Rodriguez <erickrod54@gmail.com>
2022-04-18 23:47:21 +0000

    JavaScript Nuggets react version 2 - App js file - Features: Implementing react-router-dom to set every nuggets-react-version Component as a Route

--- nuggets-react-app/src/App.js
+++ nuggets-react-app/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import UniqueValues from './components/unique-values-component';
-import './App.css';
+import { useGlobalContext } from './context';
+import Error from './error'
+import Home from './home';
+import MapMethod from './components/map-method-component';
+import  UniqueValues  from './components/unique-values-component';
+import FilterAndFind from './components/filterAndfind-method.component';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-/**Nuggets react version 1 - unique values - Features: 
+import NavBar from './navbar';
+
+/**Nuggets react version 2 - App js file- Features: 
  * 
- *     ---->Mounting UniqueValues 
- *          Component 
+ *     ---->Implementing 'react-router-dom' to
+ *          set every nuggets-react-version 
+ *          Components as Route 
  * 
  */
 
@@ -13,9 +21,16 @@ function App() {
 
   return (
     <div className="App">
-      <>
-        <UniqueValues />
-      </>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/'  element={<Home />}></Route>
+          <Route path='/map-method' element={<MapMethod />}/>
+          <Route path='/unique-values'  element={<UniqueValues />}/>
+          <Route path='/filter-and-find'  element={<FilterAndFind />}/>
+          <Route path='*'  element={<Error />}/>          
+        </Routes>
+      </Router>
     </div>
   );
 }
