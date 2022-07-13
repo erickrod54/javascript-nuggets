6f2b11d5038ff5419ce8450fe85a26fb3cf3dcdc
Erick Rodriguez <erickrod54@gmail.com>
2022-07-13 17:02:14 +0000

    Nuggets-react-app version 4 - map-method-component - Features: Fixing the context file version 4, Fixing the useNuggetsContext hook to use it on the Component that i need to provide features and data

--- nuggets-react-app/src/context.js
+++ nuggets-react-app/src/context.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { data } from './data';
+
+/**Nuggets-react-app version 4 - map-mehod-compoent 
+ * - Features:
+ * 
+ *      --> Fixing the 'context' file version
+ *          on 4. 
+ * 
+ *      --> Fixing the 'useNuggetsContext' hook to
+ *          use it on the Component that i
+ *          need to provide features and data.
+ * 
+ * Note: 'useNuggetsContext' is going to be use
+ * to save lines of code just writting the 
+ * 'useContext' hook once.
+ */
 
 /**Making the 'NuggetsContext' i'll have access
  * to the 'Consumer' and 'Provider' method */
@@ -18,5 +33,5 @@ export const NuggetsProvider = ({ children }) => {
 }
 
 export const useNuggetsContext = () => {
-    React.useContext(NuggetsContext);
+    return useContext(NuggetsContext)
 }
