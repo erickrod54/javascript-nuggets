2731c2b139179c71f86c34d2cdffe2961b99b6b7
Erick Rodriguez <erickrod54@gmail.com>
2022-10-20 10:26:38 -0400

    JavaScript-Nuggets-app version 4 - FilterAndFind - Features: Stays in the same version, ni major changes, Clearing Bugs and error related with the key prop

--- nuggets-react-app/src/components/filterAndfind-method.component.jsx
+++ nuggets-react-app/src/components/filterAndfind-method.component.jsx
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
 import { OrSomePeople } from '../data'
 
+/**JavaScript-Nuggets-app version 4 - 
+ * FilterAndFind - Features: 
+ * 
+ *       --> Stays in the same version
+ *           no major changes.
+ * 
+ *       --> Cleaning bugs and errors
+ *           related with the key prop.
+ * 
+ * Note: next versions i'll refactor the
+ * whole app to include all the methods
+ * and examples
+ */
+
 const FilterAndFind = () => {
   const [ showyounger, setShowyounger ] = useState(false);
   const [ showposition, setShowposition ] = useState(false);
@@ -28,38 +42,37 @@ const FilterAndFind = () => {
           { showyounger && OrSomePeople.filter((person) => person.age < 40).map((youngPeople) =>{
             const { name, age, position } = youngPeople;
             return(
-              <>
+              <div key={age}>
                 <h2>the people under 40 is:</h2>
                 <h2>name:{name} , age:{age}</h2>
-              </>
+                <h3>position:{position}</h3>
+              </div>
             )
           })}
           <button onClick={showPosition}>Position 'Surgical Tech'</button>
           {showposition && OrSomePeople.filter((person) => person.position === 'Surgical Tech').map((positionPeople) =>{
             const { name, age, position } = positionPeople;
             return(
-              <>
+              <div key={age}>
                 <h2>Position 'Surgical Tech':</h2>
                 <h2>name: {name} , position: {position}</h2>
-              </>
+              </div>
             )
           })}
           {/** fix to show an error message*/}
           <button onClick={showManager}>Show Manager</button>
           {showmanager && OrSomePeople.filter((person) => person.position === 'Manager').map((positionPeople) =>{
             const { name, age, position } = positionPeople;
-            if (positionPeople) {
-              return(
-                <>
+            console.log('the position ==>', position)
+            return(
+              <div key={age}>
                   <h2>Position 'Manager':</h2>
                   <h2>name: {name} , position: {position}</h2>
-                </>
-              )
-            }else{
-              <h2>Sorry does no exist</h2>
-            }
-            
-          })}
+                </div>
+              )            
+            })
+          }
+
         </>
     )
 }
