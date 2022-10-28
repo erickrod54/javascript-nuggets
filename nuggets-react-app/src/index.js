c2ce4a19ecb086c2818fe0c7d2c2aafcced92060
Erick Rodriguez <erickrod54@gmail.com>
2022-10-28 11:04:41 -0400

    Nuggets react version 5 - index js - Features: Importing NuggetsProvider and wrapping the App

--- nuggets-react-app/src/index.js
+++ nuggets-react-app/src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { NuggetsProvider } from './context';
+
+/**Nuggets react version 5 - index js - 
+ * Features: 
+ * 
+ *     --> Importing 'NuggetsProvider' and 
+ *         wrapping the 'App'
+ * 
+ * Note: By this version errors and warnings are 
+ * cleared, so i can start the refactor
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NuggetsProvider>
+      <App />
+    </NuggetsProvider>
   </React.StrictMode>
 );
 
