67c9d33cf149d5e6d8951854ca09763fc16803a5
Erick Rodriguez <erickrod54@gmail.com>
2022-10-28 11:03:39 -0400

    Nuggets react - version 5 - ErrorComponent - Features: Building ErrorComponent and displaying an error message

---
+++ nuggets-react-app/src/error.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**Nuggets react version 5 - ErrorComponent - 
+ * Features: 
+ * 
+ *     --> Building 'ErrorComponent' and 
+ *          displaying an error message 
+ * 
+ * Note: By this version errors and warnings are 
+ * cleared, so i can start the refactor
+ */
+
+const ErrorComponent = () => {
+
+    return(
+        <h2>The page you're looking for doesn't exist</h2>
+    )
+}
+
+export default ErrorComponent;
