380ffecd0f5ef03e36da1a557bb0c18852b2378b
Erick Rodriguez <erickrod54@gmail.com>
2022-10-31 01:07:13 -0400

    Nuggets react version 6 - Home - Features: Implementing useLocation hook to conditionally render a home message, Building OutletWrapper style Component to make a container for the data

--- nuggets-react-app/src/pages/home.js
+++ nuggets-react-app/src/pages/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import MainTitle from "../components/main.title";
 import WhereAmI from "../components/whereami.component";
@@ -8,19 +8,23 @@ import NavBar from "../navbar";
 /**Nuggets react version 6 - Home - 
  * Features: 
  * 
- *     --> Refactoring 'Home' Page.
- *  
- *     --> Importing and placing 
- *         Components
+ *     --> Implementing 'useLocation'
+ *         hook to conditionally render
+ *         a home message.
  * 
- *     --> Building 'HomeWrapper'
- *          style Component.
+ *     --> Building 'OutletWrapper' style
+ *         Component to make a container
+ *          for the outlet data.
  * 
  * Note: This component will show the 
  * navigation location of the user
  */
 
 const Home = () => {
+
+    const location = useLocation()
+
+    const { pathname } = location;
 
     return(
         <HomeWrapper>
@@ -30,7 +34,18 @@ const Home = () => {
                 </Link>
             }/>
             <NavBar />
+            { pathname === '/' ? 
+            <h2>JS nuggets react version is the collelction
+                of vanilla JavaScript methods that are made 
+                to solve efficientlly problems
+            </h2>
+            :
+            null 
+            }
+            
+            <OutletWrapper>
               <Outlet />
+            </OutletWrapper>
             <WhereAmI />
         </HomeWrapper>
         
@@ -43,7 +58,16 @@ const HomeWrapper = styled.div`
     margin: 2rem;
     padding: 2rem;
 
-   
+    h2{
+        display: flex;
+        justify-content: center;
+        align-content: center;
+    }
+`
+
+const OutletWrapper = styled.section`
+    width: 30rem;
+    height: 40rem;
 `
 
 export default Home;
