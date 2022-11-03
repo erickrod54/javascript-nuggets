5a0752bed66f1492847f63eb800b7514e6b08162
Erick Rodriguez <erickrod54@gmail.com>
2022-11-03 12:18:49 -0400

    JavaScript-Nuggets-react-version 11 - components > index js - Features: Importing and Exporting ReduceMethod

--- nuggets-react-app/src/components/index.components.js
+++ nuggets-react-app/src/components/index.components.js
@@ -1,5 +1,16 @@
 import FilterAndFind from "./filterAndFind-method.component";
 import MapMethod from "./map-method.component";
+import ReduceMethod from "./reduce-method.component";
 import UniqueValues from "./unique-values.component";
 
-export { FilterAndFind, MapMethod, UniqueValues}
+/**Nuggets react version 11 - components
+ * > index js - Features: 
+ * 
+ *     --> Importing and Exporting
+ *         'ReduceMethod'.
+ * 
+ * Note: This component will show the 
+ * navigation location of the user
+ */
+
+export { FilterAndFind, MapMethod, UniqueValues, ReduceMethod}
