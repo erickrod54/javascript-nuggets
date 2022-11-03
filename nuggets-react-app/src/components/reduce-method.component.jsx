2f9fca31550a01aa4cc18255ff16ba86a19a8833
Erick Rodriguez <erickrod54@gmail.com>
2022-11-03 12:22:30 -0400

    JavaScript-Nuggets-react app version 11 - ReduceMethod - Features: Building basic ReduceMethod Component

---
+++ nuggets-react-app/src/components/reduce-method.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**Nuggets react version 11 - ReduceMethod - 
+ * Features: 
+ * 
+ *     --> Building basic 'ReduceMethod'
+ *          Component
+ * 
+ * Note: This component will show the 
+ * navigation location of the user
+ */
+
+const ReduceMethod = () => {
+
+    return(
+        <h2>Reduce method component</h2>
+    )
+}
+
+export default ReduceMethod;
