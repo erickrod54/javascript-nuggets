e9aa710bbb0f4cbe54f2f04bd14b66387185afbf
Erick Rodriguez <erickrod54@gmail.com>
2022-11-04 13:28:08 -0400

    JavaScript-Nuggets-react-app version 12 - NavBar - Features: Refactoring NavBar to get the links dinamicly

--- nuggets-react-app/src/navbar.js
+++ nuggets-react-app/src/navbar.js
@@ -1,49 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useNuggetsContext } from "./context";
 
-
-/**JavaScrip nuggets React version 6 - NavBar -
+/**JavaScrip nuggets React version 12 - NavBar -
  * Features:
  * 
- *  --> Removing 'useLocation' hook to it's own 
- *      component.
+ *  --> Refactoring 'NavBar' to get the links
+ *      dinamicly 
  * 
- * Note: this approach has static links
+ * Note: this component is going to be refactored
+ * for the next versions
 */
 
 const NavBar = () => {
 
+    const { linksData } = useNuggetsContext()
+
+    console.log('testing links data ==> ', linksData)
+
     return(
+        <>
+        <h2>Methods:</h2>
        <NavBarWrapper>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/map-method'>Map method</Link>                    
-                </li>
-                <li>
-                    <Link to='/unique-values'>Unique Values</Link>                    
-                </li>
-                <li>
-                    <Link to='/filter-and-find'>Filter and find method</Link>                    
-                </li>
-            </ul>
-            
-        </nav>
-        
+
+            {linksData.map((link) => { 
+                const { id, url, name } = link;
+
+                return(
+                    <ul key={id}>
+                        <Link to={url}>
+                            {name}
+                        </Link>
+                    </ul>
+                )
+            })}
        </NavBarWrapper>
+        </>
        
     )
 }
 
 const NavBarWrapper = styled.div`
+display: flex;
+justify-content: space-around;
+
     ul{
         display: flex;
-        justify-content: space-around;
         list-style-type: none;
+        margin: 10px;
+        padding: 10px;
     }
 
     li{
+        flex-shrink: 0;
         text-decoration: none;
     }
 `
