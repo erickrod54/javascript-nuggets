20a84316a6069f283c0923c77dd4580782c939f1
Erick Rodriguez <erickrod54@gmail.com>
2022-11-04 13:29:26 -0400

    JavaScript-Nuggets-app version 12 - FilterAndFind - Features: Refactoring FilterAndFind -pending to add styles-

--- nuggets-react-app/src/components/filterAndfind-method.component.jsx
+++ nuggets-react-app/src/components/filterAndfind-method.component.jsx
@@ -1,80 +1,78 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { OrSomePeople } from '../data'
 
-/**JavaScript-Nuggets-app version 4 - 
+/**JavaScript-Nuggets-app version 12 - 
  * FilterAndFind - Features: 
  * 
- *       --> Stays in the same version
- *           no major changes.
+ *       --> Refactoring 'FilterAndFind'.
  * 
- *       --> Cleaning bugs and errors
- *           related with the key prop.
- * 
- * Note: next versions i'll refactor the
- * whole app to include all the methods
- * and examples
+ * Note: Pending to improve styles
  */
 
 const FilterAndFind = () => {
-  const [ showyounger, setShowyounger ] = useState(false);
-  const [ showposition, setShowposition ] = useState(false);
-  const [ showmanager, setShowmanager ] = useState(false);
-  
-  const youngPeople = () => {
-    setShowyounger(!showyounger)
-  }
+  const [ filterby, setFilterBy ] = useState({
+    peopleUnder40: 40,
+    position:'Surgical Tech',
+    manager:''
+  })
 
-  const showPosition = () => {
-    setShowposition(!showposition)
-  }
+  const handleFilterby = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-  const showManager = () => {
-    setShowmanager(!showmanager)
+    console.log('filter => name selected ==>', name, ', value in it ==>', value)
+    
+    setFilterBy({...filterby, [name]:value })
   }
 
     return(
         <>
+          <FilterAndFindWrapper>
           <h2>Filter and Find Component</h2>
-          <button onClick={youngPeople}>
-            People under 40
-          </button>
-          
-          { showyounger && OrSomePeople.filter((person) => person.age < 40).map((youngPeople) =>{
-            const { name, age, position } = youngPeople;
-            return(
-              <div key={age}>
-                <h2>the people under 40 is:</h2>
-                <h2>name:{name} , age:{age}</h2>
-                <h3>position:{position}</h3>
-              </div>
-            )
-          })}
-          <button onClick={showPosition}>Position 'Surgical Tech'</button>
-          {showposition && OrSomePeople.filter((person) => person.position === 'Surgical Tech').map((positionPeople) =>{
-            const { name, age, position } = positionPeople;
-            return(
-              <div key={age}>
-                <h2>Position 'Surgical Tech':</h2>
-                <h2>name: {name} , position: {position}</h2>
-              </div>
-            )
-          })}
-          {/** fix to show an error message*/}
-          <button onClick={showManager}>Show Manager</button>
-          {showmanager && OrSomePeople.filter((person) => person.position === 'Manager').map((positionPeople) =>{
-            const { name, age, position } = positionPeople;
-            console.log('the position ==>', position)
-            return(
-              <div key={age}>
-                  <h2>Position 'Manager':</h2>
-                  <h2>name: {name} , position: {position}</h2>
-                </div>
-              )            
-            })
-          }
+            <select
+              name='selection'
+              id='selection'
+              value={filterby}
+              onChange={handleFilterby}
+            >
+              <option value="">select a filter</option>
+              <option value={40}>filter people under 40</option>
+              <option value="Surgical Tech">filter Surgical Tech</option>
+            </select>
+
+            {OrSomePeople.filter((item) => item.age < filterby.selection).map((under40) => {
+                const { name, age } = under40
+                return(
+                  <div key={age}>
+                    <h2>{name}</h2>
+                    <p>{age}</p>
+                  </div>
+              )
+              })}
+
+            {OrSomePeople.filter((item) => item.position === filterby.selection).map((positions) => {
+                const { name, age, position } = positions
+                return(
+                  <div key={age}>
+                    <h2>{name}</h2>
+                    <p>{position}</p>
+                    <p>{age}</p>
+                  </div>
+              )
+              })}
+
+          </FilterAndFindWrapper>
 
         </>
     )
 }
+
+const FilterAndFindWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+
+`
 
 export default FilterAndFind;
