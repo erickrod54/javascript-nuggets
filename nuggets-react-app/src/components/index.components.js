a92430124aefe697db2b8c067eb13c7362e4bf14
Erick Rodriguez <erickrod54@gmail.com>
2022-11-07 12:04:17 -0500

    Nuggets react version 13 - components > index js - Features: Importing and Exporting ArrayDetructuring

--- nuggets-react-app/src/components/index.components.js
+++ nuggets-react-app/src/components/index.components.js
@@ -1,16 +1,17 @@
+import ArrayDestructuring from "./array.destructuring.component";
 import FilterAndFind from "./filterAndFind-method.component";
 import MapMethod from "./map-method.component";
 import ReduceMethod from "./reduce-method.component";
 import UniqueValues from "./unique-values.component";
 
-/**Nuggets react version 11 - components
+/**Nuggets react version 13 - components
  * > index js - Features: 
  * 
  *     --> Importing and Exporting
- *         'ReduceMethod'.
+ *         'ArrayDestructuring'.
  * 
  * Note: This component will show the 
  * navigation location of the user
  */
 
-export { FilterAndFind, MapMethod, UniqueValues, ReduceMethod}
+export { FilterAndFind, MapMethod, UniqueValues, ReduceMethod, ArrayDestructuring}
