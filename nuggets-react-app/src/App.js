472c742ea5cf86e1cac2f1f4d863a7441206b0fe
Erick Rodriguez <erickrod54@gmail.com>
2022-11-06 21:21:21 -0500

    Nuggets react version 13 - App js file - Features: Importing and placing ArrayDestructuring and building a route fro it

--- nuggets-react-app/src/App.js
+++ nuggets-react-app/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ErrorComponent from './error'
 import Home from './pages/home'
-import { FilterAndFind, MapMethod, ReduceMethod, UniqueValues } from './components/index.components'
+import { ArrayDestructuring, FilterAndFind, MapMethod, ReduceMethod, UniqueValues } from './components/index.components'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-/**Nuggets react version 11 - App js file- Features: 
+/**Nuggets react version 13 - App js file- Features: 
  * 
- *     --> Importing and placing 'ReduceMethod'
+ *     --> Importing and placing 'ArrayDestructuring'
  *         and building a route for it. 
  * 
  * Note: by this version i'll work refactoring
  * navbar to add reduce-method and the future 
  * methods
  */
+
 
 function App() {
 
@@ -25,7 +26,8 @@ function App() {
             <Route path='/map-method' element={<MapMethod />}/>
             <Route path='/unique-values'  element={<UniqueValues />}/>
             <Route path='/filter-and-find'  element={<FilterAndFind />}/>
-            <Route path='/reduce-method' element={<ReduceMethod />}/>          
+            <Route path='/reduce-method' element={<ReduceMethod />}/>  
+            <Route path='/array-destructuring' element={<ArrayDestructuring />}/>          
             <Route path='*'  element={<ErrorComponent />}/>
           </Route>
         </Routes>
