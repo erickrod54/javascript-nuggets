9f7a3c96239bb4772217a4a483a8032105ccdeb6
Erick Rodriguez <erickrod54@gmail.com>
2022-11-13 23:04:04 -0500

    Nuggets react version 16 - App js file - Features: Importing and placing SpreadAndRest and building a route for it

--- nuggets-react-app/src/App.js
+++ nuggets-react-app/src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ErrorComponent from './error'
 import Home from './pages/home'
-import { ArrayDestructuring, FilterAndFind, MapMethod, ReduceMethod, UniqueValues } from './components/index.components'
+import { ArrayDestructuring, FilterAndFind, MapMethod, ReduceMethod, SpreadAndRest, UniqueValues } from './components/index.components'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-/**Nuggets react version 13 - App js file- Features: 
+/**Nuggets react version 16 - App js file- Features: 
  * 
- *     --> Importing and placing 'ArrayDestructuring'
+ *     --> Importing and placing 'SpreadAndRest'
  *         and building a route for it. 
  * 
  * Note: by this version i'll work refactoring
  * navbar to add reduce-method and the future 
  * methods
  */
-
 
 function App() {
 
@@ -27,7 +26,8 @@ function App() {
             <Route path='/unique-values'  element={<UniqueValues />}/>
             <Route path='/filter-and-find'  element={<FilterAndFind />}/>
             <Route path='/reduce-method' element={<ReduceMethod />}/>  
-            <Route path='/array-destructuring' element={<ArrayDestructuring />}/>          
+            <Route path='/array-destructuring' element={<ArrayDestructuring />}/>
+            <Route path='/spread-and-rest-operator' element={<SpreadAndRest />}/>          
             <Route path='*'  element={<ErrorComponent />}/>
           </Route>
         </Routes>
