e13f261e580f9a2704976289599c90985a906acd
Erick Rodriguez <erickrod54@gmail.com>
2022-11-13 23:05:52 -0500

    Nuggets-react-version 16 - components > index js - Features: Importing and Exporting SpreadAndRest

--- nuggets-react-app/src/components/index.components.js
+++ nuggets-react-app/src/components/index.components.js
@@ -2,16 +2,17 @@ import ArrayDestructuring from "./array.destructuring.component";
 import FilterAndFind from "./filterAndFind-method.component";
 import MapMethod from "./map-method.component";
 import ReduceMethod from "./reduce-method.component";
+import SpreadAndRest from "./spread.and.rest.operator.component";
 import UniqueValues from "./unique-values.component";
 
-/**Nuggets react version 13 - components
+/**Nuggets react version 16 - components
  * > index js - Features: 
  * 
  *     --> Importing and Exporting
- *         'ArrayDestructuring'.
+ *         'SpreadAndRest'.
  * 
  * Note: This component will show the 
  * navigation location of the user
  */
 
-export { FilterAndFind, MapMethod, UniqueValues, ReduceMethod, ArrayDestructuring}
+export { FilterAndFind, MapMethod, UniqueValues, ReduceMethod, ArrayDestructuring, SpreadAndRest}
