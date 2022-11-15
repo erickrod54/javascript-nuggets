1dda352192d584bec0b08074178d6b279369adac
Erick Rodriguez <erickrod54@gmail.com>
2022-11-15 08:34:10 -0500

    Nuggets-react-version 18 - components > index js - Features: Importing and Exporting ArrayFrom

--- nuggets-react-app/src/components/index.components.js
+++ nuggets-react-app/src/components/index.components.js
@@ -1,18 +1,25 @@
 import ArrayDestructuring from "./array.destructuring.component";
+import ArrayFrom from "./array.from.component";
 import FilterAndFind from "./filterAndFind-method.component";
 import MapMethod from "./map-method.component";
 import ReduceMethod from "./reduce-method.component";
 import SpreadAndRest from "./spread.and.rest.operator.component";
 import UniqueValues from "./unique-values.component";
 
-/**Nuggets react version 16 - components
+/**Nuggets react version 18 - components
  * > index js - Features: 
  * 
  *     --> Importing and Exporting
- *         'SpreadAndRest'.
+ *         'ArrayFrom'.
  * 
  * Note: This component will show the 
  * navigation location of the user
  */
 
-export { FilterAndFind, MapMethod, UniqueValues, ReduceMethod, ArrayDestructuring, SpreadAndRest}
+export { FilterAndFind,
+         MapMethod,
+         UniqueValues,
+         ReduceMethod,
+         ArrayDestructuring,
+         SpreadAndRest,
+         ArrayFrom}
