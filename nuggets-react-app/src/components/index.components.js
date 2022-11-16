4a79c60b56ea0c0b6559c934eb6140dd8a73950c
Erick Rodriguez <erickrod54@gmail.com>
2022-11-16 00:21:52 -0500

    Nuggets-react-version 19 - components > index js - Features: Importing and Exporting OptionalChaining

--- nuggets-react-app/src/components/index.components.js
+++ nuggets-react-app/src/components/index.components.js
@@ -2,19 +2,22 @@ import ArrayDestructuring from "./array.destructuring.component";
 import ArrayFrom from "./array.from.component";
 import FilterAndFind from "./filterAndFind-method.component";
 import MapMethod from "./map-method.component";
+import OptionalChaining from "./optional.chaining.component";
 import ReduceMethod from "./reduce-method.component";
 import SpreadAndRest from "./spread.and.rest.operator.component";
 import UniqueValues from "./unique-values.component";
 
-/**Nuggets react version 18 - components
+/**Nuggets react version 19 - components
  * > index js - Features: 
  * 
  *     --> Importing and Exporting
- *         'ArrayFrom'.
+ *         'OptionalChaining'.
  * 
  * Note: This component will show the 
  * navigation location of the user
  */
+
+
 
 export { FilterAndFind,
          MapMethod,
@@ -22,4 +25,6 @@ export { FilterAndFind,
          ReduceMethod,
          ArrayDestructuring,
          SpreadAndRest,
-         ArrayFrom}
+         ArrayFrom,
+         OptionalChaining   
+        }
