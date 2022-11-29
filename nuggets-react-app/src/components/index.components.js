64d1ad388f32297bfdb377ef0a45054ad5179188
Erick Rodriguez <erickrod54@gmail.com>
2022-11-29 12:59:31 -0500

    Nuggets react version 23 - components > index js  - Features: Importing and exporting CallbackFunction

--- nuggets-react-app/src/components/index.components.js
+++ nuggets-react-app/src/components/index.components.js
@@ -1,5 +1,6 @@
 import ArrayDestructuring from "./array.destructuring.component";
 import ArrayFrom from "./array.from.component";
+import CallbackFunction from "./callback.function.component";
 import FilterAndFind from "./filterAndFind-method.component";
 import MapMethod from "./map-method.component";
 import OptionalChaining from "./optional.chaining.component";
@@ -7,24 +8,24 @@ import ReduceMethod from "./reduce-method.component";
 import SpreadAndRest from "./spread.and.rest.operator.component";
 import UniqueValues from "./unique-values.component";
 
-/**Nuggets react version 19 - components
+/**Nuggets react version 23 - components
  * > index js - Features: 
  * 
  *     --> Importing and Exporting
- *         'OptionalChaining'.
+ *         'CallbackFunction'.
  * 
  * Note: This component will show the 
  * navigation location of the user
  */
 
 
-
 export { FilterAndFind,
-         MapMethod,
-         UniqueValues,
-         ReduceMethod,
-         ArrayDestructuring,
-         SpreadAndRest,
-         ArrayFrom,
-         OptionalChaining   
+        MapMethod,
+        UniqueValues,
+        ReduceMethod,
+        ArrayDestructuring,
+        SpreadAndRest,
+        ArrayFrom,
+        OptionalChaining,   
+        CallbackFunction
         }
